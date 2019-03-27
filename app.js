4fd491afed2adf68f6d0b75863e448e454bcf511
Chris Carr <christocarr@outlook.com>
2019-03-27 20:11:11 +0000

    Add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb', {useNewUrlParser: true});
+let db = mongoose.connection;
+
+//check connection
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+//check for DB errors
+db.on('error', () => {
+  console.log(err);
+});
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(3000, () => {
